b65249515c95411d369453f4f56a93ac2a4aa0ac
BriacRiaud <briac.riaud@supdevinci-edu.fr>
2024-11-27 10:53:11 +0100

    image

--- blolgmon/app/components/MainContent.tsx
+++ blolgmon/app/components/MainContent.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Image from 'next/image';
 
 const MainContent = () => {
   return (
@@ -6,12 +7,12 @@ const MainContent = () => {
       <h1 className="text-3xl font-bold text-center mb-5">Category: Business</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
         <article className="bg-white shadow-lg rounded p-5">
-          <img src="img/news-700x435-1.jpg" alt="News" className="w-full h-64 object-cover rounded mb-3" />
+          <Image src="/temu gnar.webp" width={(1024)} height={(1024)} alt="News" className="w-full h-64 object-cover rounded mb-3" />
           <h2 className="text-xl font-bold mb-2">Lorem ipsum dolor sit amet elit...</h2>
           <p className="text-gray-600">Dolor lorem eos dolor duo et eirmod sea. Dolor sit magna...</p>
         </article>
         <article className="bg-white shadow-lg rounded p-5">
-          <img src="img/news-700x435-2.jpg" alt="News" className="w-full h-64 object-cover rounded mb-3" />
+        <Image src="/malphite.png" width={(1024)} height={(1024)} alt="News" className="w-full h-64 object-cover rounded mb-3" />
           <h2 className="text-xl font-bold mb-2">Dolor lorem eos dolor duo...</h2>
           <p className="text-gray-600">Dolor sit magna rebum clita rebum dolor stet amet justo...</p>
         </article>
